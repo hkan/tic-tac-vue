ff0dfd5fd17c2e0a3726b81b8436af2b76e39233
Hakan <iletisim@hakanaktas.com.tr>
2016-08-10 20:29:56 +0300

    missing Vue component

---
+++ resources/assets/js/components/Game.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="game">
+        <tic-tac-toe></tic-tac-toe>
+    </div>
+</template>
+
+<script>
+    import TicTacToe from './TicTacToe.vue'
+    export default {
+        components: {
+            TicTacToe,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
